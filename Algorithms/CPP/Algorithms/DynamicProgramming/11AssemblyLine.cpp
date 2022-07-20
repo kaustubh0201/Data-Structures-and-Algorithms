9c5c1e270d2d29b028a8c856ae12aaa5989e687a
Kaustubh Shrivastava <kaustubh1818@gmail.com>
2022-07-20 20:13:51 +0530

    Assembly Line Scheduling

--- Algorithms/CPP/Algorithms/DynamicProgramming/11AssemblyLine.cpp
+++ Algorithms/CPP/Algorithms/DynamicProgramming/11AssemblyLine.cpp
@@ -1,10 +1,32 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 
 using namespace std;
 
-int assemblyLineScheduling(vector<vector<int>> &assembleTime, vector<int> &turningTime, vector<int> &entryTime, vector<int> &exitTime){
+int assemblyUtil(vector<vector<int>> &assembleTime, vector<vector<int>> &turningTime, vector<int> &entryTime, vector<int> &exitTime){
+
+    int n = assembleTime[0].size();
+
+    vector<int> T1(n), T2(n);
+
+    T1[0] = entryTime[0] + assembleTime[0][0];
+    T2[0] = entryTime[1] + assembleTime[1][0];
+
+    for(int i = 1; i < n; i++){
+
+        T1[i] = min(T1[i - 1] + assembleTime[0][i], T1[i - 1] + assembleTime[0][i] + turningTime[1][i]);
+        T2[i] = min(T2[i - 1] + assembleTime[0][i], T2[i - 1] + assembleTime[1][i] + turningTime[0][i]);
+
+    }
+
+    return min(T1[n - 1] + exitTime[0], T2[n - 1] + exitTime[1]);
+}
+
+int assemblyLineScheduling(vector<vector<int>> &assembleTime, vector<vector<int>> &turningTime, vector<int> &entryTime, vector<int> &exitTime){
     
+    return assemblyUtil(assembleTime, turningTime, entryTime, exitTime);
+
 }
 
 int main(void){
@@ -14,6 +36,7 @@ int main(void){
     vector<int> entryTime = {10, 12};
     vector<int> exitTime = {18, 7};
 
+    cout << assemblyLineScheduling(assembleTime, turningTime, entryTime, exitTime) << endl;
 
     return 0;
 }
